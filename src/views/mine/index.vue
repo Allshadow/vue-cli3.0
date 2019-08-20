9e81116e4491fc8a972a8813644017a92d829875
Allshadow <490178988@qq.com>
2019-08-20 15:19:29 +0800

    static alrady

---
+++ src/views/mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="mine">
+    <Header title="我的喵喵"/>
+    <div id="content">
+      <Login />
+    </div>
+    <TabBar/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import TabBar from '@/components/TabBar.vue'
+import Login from '@/components/Login.vue'
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    TabBar,
+    Login
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
